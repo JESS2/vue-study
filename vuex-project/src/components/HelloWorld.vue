c2e25bda498234f9e6a563479c9bc51623dbbecd
Soyeon Jung <jess2.jsy@gmail.com>
2018-09-11 15:40:59 +0900

    vuex 학습을 위한 프로젝트 생성 및 초기 구성

---
+++ vuex-project/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h2>Vuex 상태 관리 라이브러리</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
